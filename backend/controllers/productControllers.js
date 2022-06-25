7c1932c3e296e7287fd33074ae6279b09882fc10
software369 <83788939+software369@users.noreply.github.com>
2022-06-25 02:00:27 -0500

    Initial REST API

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -1,10 +1,14 @@
 const asyncHandler = require('express-async-handler')
 
+const Product = require('../models/productModel')
+
 // Get Products
 // GET /api/products
 // Public
 const getProducts = asyncHandler(async(req, res) => {
-    res.status(200).json({Message: 'Get Products'})
+    const products = await Product.find()
+    
+    res.status(200).json(products)
 })
 
 // Set Product
@@ -16,21 +20,45 @@ const setProduct = asyncHandler(async(req, res) => {
         throw new Error('Please add a text field')
     }
 
-    res.status(200).json({Message: 'Set Product'})
+    const product = await Product.create({
+        text: req.body.text
+    })
+
+    res.status(200).json(product)
 })
 
 // Update Product
 // PUT /api/products/:id
 // Public
 const updateProduct = asyncHandler(async(req, res) => {
-    res.status(200).json({Message: `Update Product ${req.params.id}`})
+    const product = await Product.findById(req.params.id)
+
+    if(!product){
+        res.status(400)
+        throw new Error('Product not found')
+    }
+
+    const updatedProduct = await Product.findByIdAndUpdate(req.params.id, req.body, {
+        new: true
+    })
+
+    res.status(200).json(updatedProduct)
 })
 
 // Delete Product
 // DELETE /api/products/:id
 // Public
 const deleteProduct = asyncHandler(async(req, res) => {
-    res.status(200).json({Message: `Delete Product ${req.params.id}`})
+    const product = await Product.findById(req.params.id)
+
+    if(!product){
+        res.status(400)
+        throw new Error('Product not found')
+    }
+
+    await product.remove()
+
+    res.status(200).json({id: req.params.id})
 })
 
 module.exports = {
